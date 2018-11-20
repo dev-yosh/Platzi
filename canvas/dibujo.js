c28a661f42c9f8abb798884163309c575092d0b8
Jose Manuel <ciscojose1107@gmail.com>
2018-11-19 23:13:07 -0600

    canvas3

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,10 +2,43 @@ var d = document.getElementById("dibujito")
 var lienzo = d.getContext("2d");
 
 var dx = 0;
+var anchuraLineas = 10;
+var lineas = 0;
 
-for (var i = 0; i < 1000; i+=10) {
-  dibujarLinea("red", 10+i, 300, 150, 50);
+dx = 0;
+lineas = 0;
+while (lineas < 30) {
+  dibujarLinea("red", 0, dx, 10+dx, 300);
+  dx += anchuraLineas;
+  lineas++;
 }
+
+dx = 0;
+lineas = 0;
+while (lineas < 30) {
+  dibujarLinea("orange", 0, dx , 300-dx, 0);
+  dx += anchuraLineas;
+  lineas++;
+}
+
+dx = 0;
+lineas = 0;
+while (lineas < 30) {
+  dibujarLinea("blue", 300, 300-dx, 290-dx, 0);
+  dx += anchuraLineas;
+  lineas++;
+}
+
+dx = 0;
+lineas = 0;
+while (lineas < 30) {
+  dibujarLinea("green", 300, 300-dx, dx, 300);
+  dx += anchuraLineas;
+  lineas++;
+}
+
+
+
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
